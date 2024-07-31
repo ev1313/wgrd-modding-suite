ff6f361cbfa2ead3b22936e8018abb592f64c7ff
Tim Blume <git@3nd.io>
2024-07-31 23:15:59 +0200

    test

--- ci-build.sh
+++ ci-build.sh
@@ -1,6 +1,7 @@
 cmake -DCMAKE_PREFIX_PATH="/mingw64/" -DCMAKE_MODULE_PATH="$(pwd)/modules/" -DWIN32=ON -B build/
 cmake --build build/ -j8
-find /ucrt64 -name "*python*dll"
+find build/venv/ -name "*python*"
+find /ucrt64 -name "*python*"
 cp /ucrt64/bin/libepoxy-0.dll build/
 cp /ucrt64/bin/glfw3.dll build/
-cp /ucrt64/bin/libpython3.*.dll build/
+cp /ucrt64/bin/libpython3*.dll build/
