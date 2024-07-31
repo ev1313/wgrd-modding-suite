eab03116c85b12077e912a0b2db699c3d5a096e9
Tim Blume <git@3nd.io>
2024-07-31 22:50:38 +0200

    ucrt fix

--- ci-build.sh
+++ ci-build.sh
@@ -1,6 +1,5 @@
 cmake -DCMAKE_PREFIX_PATH="/mingw64/" -DCMAKE_MODULE_PATH="$(pwd)/modules/" -DWIN32=ON -B build/
 cmake --build build/ -j8
-ls /
-cp /mingw64/bin/libepoxy-0.dll build/
-cp /mingw64/bin/glfw3.dll build/
-cp /mingw64/bin/libpython3.*.dll build/
+cp /ucrt64/bin/libepoxy-0.dll build/
+cp /ucrt64/bin/glfw3.dll build/
+cp /ucrt64/bin/libpython3.*.dll build/
