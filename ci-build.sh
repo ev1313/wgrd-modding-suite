358b4e45a30d438ce902ef3df520a60ad0facbe7
Tim Blume <git@3nd.io>
2024-07-31 22:29:14 +0200

    remove find

--- ci-build.sh
+++ ci-build.sh
@@ -1,8 +1,5 @@
 cmake -DCMAKE_PREFIX_PATH="/mingw64/" -DCMAKE_MODULE_PATH="$(pwd)/modules/" -DWIN32=ON -B build/
 cmake --build build/ -j8
-find /mingw64/ -name "libepoxy-0.dll"
-find /mingw64/ -name "libglfw3.dll"
-find /mingw64/ -name "libpython*.dll"
 cp /mingw64/bin/libepoxy-0.dll build/
 cp /mingw64/bin/glfw3.dll build/
 cp /mingw64/bin/libpython3.*.dll build/
