e649a13c986a7f5316bc2923c1c78ea9ac8c8235
Tim Blume <git@3nd.io>
2024-07-31 18:22:00 +0200

    lib / include

--- modules/FindEpoxy.cmake
+++ modules/FindEpoxy.cmake
@@ -4,13 +4,13 @@ libfind_pkg_check_modules(LibEpoxy_PKGCONF epoxy)
 
 find_path(LibEpoxy_INCLUDE_DIR
   NAMES epoxy/gl.h
-  HINTS ${LibEpoxy_PKGCONF_INCLUDE_DIRS}
+  HINTS ${LibEpoxy_PKGCONF_INCLUDE_DIRS} /mingw64/ /mingw64/include/
   PATH_SUFFIXES LibEpoxy
 )
 
 find_library(LibEpoxy_LIBRARY
   NAMES epoxy
-  HINTS ${LibEpoxy_PKGCONF_LIBRARY_DIRS}
+  HINTS ${LibEpoxy_PKGCONF_LIBRARY_DIRS} /mingw64/ /mingw64/lib/
 )
 
 set(LibEpoxy_PROCESS_INCLUDES LibEpoxy_INCLUDE_DIR)
