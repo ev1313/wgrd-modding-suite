b76b4117718565814ad466af1ffcad4bc28aa8c3
Tim Blume <end@3nd.io>
2024-07-31 17:15:45 +0200

    test

--- ci-build.sh
+++ ci-build.sh
@@ -1,3 +1,3 @@
 ls -hal /mingw64/lib/cmake/glfw3/
-cmake -DCMAKE_MODULE_PATH=modules/ -B build/
+cmake -DCMAKE_MODULE_PATH="/mingw64/lib/cmake/glfw3/ $(pwd)/modules/" -B build/
 cmake --build build/ -j8
