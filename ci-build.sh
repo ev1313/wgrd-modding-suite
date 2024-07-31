fbd6a51a56d6d396351e1c93d15a1a22e83f337c
Tim Blume <git@3nd.io>
2024-07-31 22:05:46 +0200

    ci-build

--- ci-build.sh
+++ ci-build.sh
@@ -1,5 +1,6 @@
 cmake -DCMAKE_PREFIX_PATH="/mingw64/" -DCMAKE_MODULE_PATH="$(pwd)/modules/" -DWIN32=ON -B build/
 cmake --build build/ -j8
+find /mingw64/ -name "libepoxy-0.dll"
 cp /mingw64/lib/libepoxy-0.dll build/
 cp /mingw64/lib/libglfw3.dll build/
 cp /mingw64/lib/libpython3.11.dll build/
