8472c541fd4f2cc6d61c2494d1293740099c43c3
Tim Blume <git@3nd.io>
2024-07-31 23:37:52 +0200

    remove find

--- ci-build.sh
+++ ci-build.sh
@@ -1,7 +1,5 @@
 cmake -DCMAKE_PREFIX_PATH="/mingw64/" -DCMAKE_MODULE_PATH="$(pwd)/modules/" -DWIN32=ON -B build/
 cmake --build build/ -j8
-find build/venv/ -name "*python*"
-find /ucrt64 -name "*python*"
 cp /ucrt64/bin/libepoxy-0.dll build/
 cp /ucrt64/bin/glfw3.dll build/
 cp /ucrt64/bin/libpython3*.dll build/
