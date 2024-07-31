3086455a359efc3a9f15a74a7817c2b08af7d5f8
Tim Blume <git@3nd.io>
2024-08-01 00:40:54 +0200

    try building out of venv

--- ci-build.sh
+++ ci-build.sh
@@ -1,3 +1,8 @@
+mkdir build
+python3 -m venv build/venv/
+source build/venv/bin/activate
+pip install wgrd-cons-parsers wgrd-cons-tools
+
 cmake -DCMAKE_PREFIX_PATH="/mingw64/" -DCMAKE_MODULE_PATH="$(pwd)/modules/" -DWIN32=ON -B build/
 cmake --build build/ -j8
 cp /ucrt64/bin/libepoxy-0.dll build/
