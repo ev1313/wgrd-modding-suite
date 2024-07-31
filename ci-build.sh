4dbea06b34b5a463a49a2e4022ba972a4a5c88e5
Your Name <you@example.com>
2024-08-01 01:37:42 +0200

    test

--- ci-build.sh
+++ ci-build.sh
@@ -5,6 +5,8 @@ pip install wgrd-cons-parsers wgrd-cons-tools
 
 cmake -DCMAKE_PREFIX_PATH="/mingw64/" -DCMAKE_MODULE_PATH="$(pwd)/modules/" -DWIN32=ON -B build/
 cmake --build build/ -j8
+cp /ucrt64/bin/libgcc_s_seh-1.dll build/
+cp /ucrt64/bin/libwinpthread-1.dll build/
 cp /ucrt64/bin/libepoxy-0.dll build/
 cp /ucrt64/bin/glfw3.dll build/
 cp /ucrt64/bin/libpython3*.dll build/
