45ccd5f7d5824a615d862ad1ceb3d0e7dbc84efb
Your Name <you@example.com>
2024-08-01 11:32:20 +0200

    reduce binary size

--- ci-build.sh
+++ ci-build.sh
@@ -6,6 +6,7 @@ pip install wgrd-cons-parsers wgrd-cons-tools
 cmake -DCMAKE_PREFIX_PATH="/mingw64/" -DCMAKE_MODULE_PATH="$(pwd)/modules/" -DWIN32=ON -B build/
 cmake --build build/ -j8
 cp -r /ucrt64/lib/python3.11/ build/venv/lib/
+find build/venv/lib/ -name "*.pyc" -delete
 cp -r fonts/ build/
 cp /ucrt64/bin/libgcc_s_seh-1.dll build/
 cp /ucrt64/bin/libwinpthread-1.dll build/
