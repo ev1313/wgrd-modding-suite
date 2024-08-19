2f5792dd2ffba40e1152f76cb4d49f5c50787a33
Your Name <you@example.com>
2024-08-20 01:52:09 +0200

    update batch file, since it was not updating deps

--- start_modding_suite.bat
+++ start_modding_suite.bat
@@ -1,5 +1,6 @@
 if not exist ".\venv\" python -m venv venv
 call .\venv\Scripts\activate
-pip install wgrd-cons-tools
+pip install wgrd-cons-parsers --upgrade
+pip install wgrd-cons-tools --upgrade
 set PYTHONPATH=.\venv\Lib\site-packages
 modding_suite.exe
